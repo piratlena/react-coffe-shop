a0e4dd81c959acd85c85841cfcdc5426290dc870
Elena Brazhnikova <lenovosibirsk@mail.ru>
2022-10-09 21:03:57 +0700

    add basic layout to  header component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,10 @@
 
-import './App.css';
+import Header from '../header/header';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="app">
+      <Header/>
     </div>
   );
 }
---
+++ src/components/header/header.js
@@ -0,0 +1,19 @@
+import './header.scss';
+import logoTwoGrains from '../../resourses/img/logo_two-grains.svg'
+const Header = () => {
+
+    return (
+        <header className='header'>
+            <img src={logoTwoGrains} className="header__logo" alt='logo'></img>
+            <nav className='header__nav'>
+                <ul className='header__list'>
+                    <li className='header__list-item'><a href="#" className='header__link'>Coffee house</a></li>
+                    <li className='header__list-item'><a href="#" className='header__link'>Our coffee</a></li>
+                    <li className='header__list-item'><a href="#" className='header__link'>For your pleasure</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+
+}
+export default Header;
